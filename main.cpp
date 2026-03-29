e5b20d23fdaec79cd65eeb2a203913458ac5b17b
Nty <natalyorozco550@gmail.com>
2026-03-29 18:22:54 -0500

    ejercicio_8_completado (#7)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include "configuracion.h"
+//#include <iostream>
+//#include "configuracion.h"
 #include "enlace.h"
 int main(){
     ejercicio_2();
+    ejercicio_8();
 
   return 0;
 }
