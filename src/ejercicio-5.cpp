a884fdc91c38388207dd0bf265d87a7d579c3979
Elmer Mosquera <elmer.mosquera@udea.edu.co>
2026-03-30 21:43:49 -0500

    ejercicio 5 terminado

--- src/ejercicio-5.cpp
+++ src/ejercicio-5.cpp
@@ -4,6 +4,31 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+void convertirIntToString(char* cadena, int numero);
+
 void ejercicio_5(){
-    cout << "Hola desde el ejercicio 5" << endl;
+    int numero;
+    char cadena[10];
+
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+    cout << "El numero ingresado es: " << numero << endl;
+
+    convertirIntToString(cadena, numero);
+    cout << "El numero ingresado como cadena es: " << cadena << endl;
+}
+
+void convertirIntToString(char* cadena, int numero){
+    int cpNumero = numero;
+    int invertido = 0;
+    int i = 0;
+    while (cpNumero > 0) {
+        invertido = invertido * 10 + cpNumero % 10;
+        cpNumero /= 10;
+    }
+    while (invertido > 0) {
+        cadena[i++] = invertido % 10 + '0';
+        invertido /= 10;
+    }
+    cadena[i] = '\0';
 }
